a8c4b4a3465b2b1b87b1eb9918bb96edb292941e
Paulo <paulo.silva1514@etec.sp.gov.br>
2023-05-24 17:58:23 -0300

    alteração

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
  main() {
 	
-printf(" Hello World\n");
-printf(" Hello Universe");
+int numero1;
+int numero2;
+
+numero1 = 10;
+numero2 = 20;
+printf("Digite um valor para o numero 1");
+scanf("%d",&numero1);
+printf("Digite um valor para o numero 2");
+scanf("%d",&numero2);
+printf("O numero 1:%d\nE o numero 2:%d",numero1,numero2);
+
 }
